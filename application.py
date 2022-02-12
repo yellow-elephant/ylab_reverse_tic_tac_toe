c13327a7517cc9bcb2b90f94d0cb3b126bcc8733
ruslan <rgimaev1984@gmail.com>
2022-02-12 17:38:43 +0500

    add random ai move

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+from random import choice
 
 app = Flask(__name__)
 
@@ -21,11 +22,10 @@ def index():
             session["board"].append(list())
             for y in range(10):
                 session["board"][x].append(None)
-        return render_template("board.html", player=session["player"], board=session["board"])
-    else:
-        if session["turn"] != session["player"]:
-            session["board"] = get_ai_move(session["board"], session["turn"])
-        return render_template("board.html", player=session["player"], board=session["board"])
+    if session["turn"] != session["player"]:
+        session["board"] = get_ai_move(session["board"], session["turn"])
+        session["turn"] = change_turn(session["turn"])
+    return render_template("board.html", player=session["player"], board=session["board"])
 
 
 @app.route("/choose/<string:player>")
@@ -37,11 +37,30 @@ def choose(player):
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
     session["board"][row][col] = session["player"]
+    session["turn"] = change_turn(session["turn"])
     return redirect(url_for("index"))
 
 
-def get_ai_move(board, turn):
+def get_possible_moves(board: list):
+    moves = list()
+    for x in range(10):
+        for y in range(10):
+            if not board[x][y]:
+                moves.append((x, y))
+    return moves
+
+
+def get_ai_move(board: list, turn: str):
+    move = choice(get_possible_moves(board))
+    board[move[0]][move[1]] = turn
     return board
+
+
+def change_turn(turn: str):
+    if turn == "X":
+        return "Y"
+    else:
+        return "X"
 
 
 if __name__ == '__main__':
